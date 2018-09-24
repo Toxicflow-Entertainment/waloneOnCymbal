4970c3f98bfbac86cbe5d13214ac0e439e4ca8e3
Filip Małecki <f.malecki.t@gmail.com>
2018-09-24 16:08:04 +0200

    first commit

---
+++ waloneOnCymbal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace waloneOnCymbal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
